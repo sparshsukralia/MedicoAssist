6c01b4f4965c2d53738cceb9c74b27b2059b6b7e
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2021-11-30 17:37:05 +0530

    sign in and sign up pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,17 @@ import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 import "./App.css";
 
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
 const apiKey = "3dgm5vhymws6";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+  if (!authToken) return <Auth />;
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
